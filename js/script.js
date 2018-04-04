a2d2e1762aff753fcaeb5109d2b787f3c7187261
DESKTOP-B1E5NQ0\Алексей <kalebro2h@gmail.com>
2018-04-04 21:44:06 +0700

    1 Исправления после проверки

--- js/script.js
+++ js/script.js
@@ -4,46 +4,45 @@ var slides = document.querySelectorAll('.slide');
 var curSlide = 0; 
 
 left.addEventListener('click', function () { 
-slide('left'); 
+	slide('left'); 
 }); 
 
 right.addEventListener('click', function () { 
-slide('right'); 
+	slide('right'); 
 }) 
 
 function slide (direction) { 
-    var count = slides.length; 
+	var count = slides.length; 
+	var nextSlide = direction === 'left' ? curSlide - 1 : curSlide + 1; 
+	nextSlide = nextSlide > count - 1 ? 0 : 
+	(nextSlide < 0 ? count - 1 : nextSlide); 
 
-    var nextSlide = direction === 'left' ? curSlide - 1 : curSlide + 1; 
-    nextSlide = nextSlide > count - 1 ? 0 : 
-    (nextSlide < 0 ? count - 1 : nextSlide); 
+	slides[nextSlide].style.display = 'block'; 
+	slides[curSlide].style.display = 'none'; 
 
-    slides[nextSlide].style.display = 'block'; 
-    slides[curSlide].style.display = 'none'; 
-
-    curSlide = nextSlide; 
+	curSlide = nextSlide; 
 } 
 
 function openTab (id, obj) {
 	var tab = document.getElementById(id);
-  var active = document.querySelector('.tab-button_active');
-  var activeTab = document.querySelector('.tab-content_active');
-  
-  active.classList.remove('tab-button_active');
-  obj.classList.add('tab-button_active');
-  
-  activeTab.classList.remove('tab-content_active');
-  tab.classList.add('tab-content_active');
+	var active = document.querySelector('.tab-button_active');
+	var activeTab = document.querySelector('.tab-content_active');
+
+	active.classList.remove('tab-button_active');
+	obj.classList.add('tab-button_active');
+
+	activeTab.classList.remove('tab-content_active');
+	tab.classList.add('tab-content_active');
 }
 
 function openModal(id) {
-    event.preventDefault();
-    var modal = document.getElementById(id);
-    modal.style.display = 'block';
-    }
+	event.preventDefault();
+	var modal = document.getElementById(id);
+	modal.style.display = 'block';
+}
 
 function closeModal(id) { 
-    var modal = document.getElementById(id); 
-    modal.style.display = 'none'; 
-    }
+	var modal = document.getElementById(id); 
+	modal.style.display = 'none'; 
+}
 
